058b4d59c159e1cb8c78856a20fb4ad2896ff22a
Yassin-pixel-bit <yasyous2021@gmail.com>
2026-03-20 04:07:54 +0200

    feat: add a constructor for fluid_container struct

--- fluid_math.h
+++ fluid_math.h
@@ -19,4 +19,18 @@ struct fluid_container
 
     // Convert 2D coordinates (x, y) to 1D array index for a grid of size (n+2) x (n+2)
     inline int IDX(int i, int j) { return j * (width + 2) + i; }
+
+    fluid_container(int Height, int Width) : height(Height), width(Width)
+    {
+        int size = (height + 2) * (width + 2);
+
+        dens.resize(size);
+        dens_prev.resize(size);
+        vel_x.resize(size);
+        vel_y.resize(size);
+        vel_x_prev.resize(size);
+        vel_y_prev.resize(size);
+    }
 };
+
+void dens_step(int boundary_t, float diff, vector<float>& emission_arr, fluid_container& container);
