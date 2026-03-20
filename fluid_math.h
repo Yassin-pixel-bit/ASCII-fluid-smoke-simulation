e22fd46f8aaaad1ff9f28e824548559d1a3144b7
Yassin-pixel-bit <yasyous2021@gmail.com>
2026-03-20 02:14:54 +0200

    refactor: encapsulate fluid state in container

--- fluid_math.h
+++ fluid_math.h
@@ -3,7 +3,20 @@
 #include <iostream>
 #include <vector>
 
-// Macros
+struct fluid_container 
+{
+    int height;
+    int width;
+    float dt;
 
-// Convert 2D coordinates (x, y) to 1D array index for a grid of size (n+2) x (n+2)
-#define IDX(x, y, stride) ((y) * (stride + 2) + (x))
+    // Grids
+    vector<float> dens;
+    vector<float> dens_prev;
+    vector<float> vel_x;
+    vector<float> vel_y;
+    vector<float> vel_x_prev;
+    vector<float> vel_y_prev;
+
+    // Convert 2D coordinates (x, y) to 1D array index for a grid of size (n+2) x (n+2)
+    inline int IDX(int i, int j) { return j * (width + 2) + i; }
+};
