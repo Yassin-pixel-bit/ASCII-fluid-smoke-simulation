522a345c699879fbd29f8647d0951c8378cf0b13
Yassin-pixel-bit <yasyous2021@gmail.com>
2026-03-22 03:41:01 +0200

    feat: integrate user settings into fluid emitters and wind controls

--- interactive.cpp
+++ interactive.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-void add_wind(fluid_container& container, const InputState& input_state)
+void add_wind(const sim_config& config, fluid_container& container, const InputState& input_state)
 {
         int center_x = container.width / 2;
         int center_y = container.height / 2;
         int top_y = 2;
 
-        float wind_force = 500.0f;
+        float wind_force = config.wind_force;
         if (input_state.wind_w) {
             container.vel_y_prev[container.IDX(center_x, container.height - 2)] = -wind_force;
         }
@@ -23,22 +23,24 @@ void add_wind(fluid_container& container, const InputState& input_state)
         }
 }
 
-void add_sources(fluid_container& container, const InputState& input_state, vector<float>& emission_arr)
+void add_sources(const sim_config& config, fluid_container& container, const InputState& input_state, vector<float>& emission_arr)
 {
-    int center_x = container.width / 2;
-    int center_y = container.height / 2;
-    int top_y = 2;
+    int fluid_x = 2 + (int)((container.width - 3) * config.spawn_x);
+    int fluid_y = 2 + (int)((container.height - 3) * config.spawn_y);
 
     if (input_state.pouring_smoke)
     {
-        // Add density
-        emission_arr[container.IDX(center_x, top_y)] = 1000.0f;
-        emission_arr[container.IDX(center_x + 1, top_y)] = 1000.0f;
-        emission_arr[container.IDX(center_x, top_y)] = 1000.0f;
+        float amount = config.fluid_amount;
+        float push = config.spawn_push;
 
-        container.vel_y_prev[container.IDX(center_x, top_y)] = 500.0f;
-        container.vel_y_prev[container.IDX(center_x - 1, top_y)] = 500.0f;
-        container.vel_y_prev[container.IDX(center_x + 1, top_y)] = 500.0f;
+        // Add density
+        emission_arr[container.IDX(fluid_x, fluid_y)] = amount;
+        emission_arr[container.IDX(fluid_x + 1, fluid_y)] = amount;
+        emission_arr[container.IDX(fluid_x - 1, fluid_y)] = amount;
+
+        container.vel_y_prev[container.IDX(fluid_x, fluid_y)] = config.spawn_y < 0.5 ? push : -push;
+        container.vel_y_prev[container.IDX(fluid_x - 1, fluid_y)] = config.spawn_y < 0.5 ? push : -push;
+        container.vel_y_prev[container.IDX(fluid_x + 1, fluid_y)] = config.spawn_y < 0.5 ? push : -push;
     }
 }
 
@@ -54,7 +56,7 @@ void update_input(InputState& input_state)
     updateActionState(input_state.wind_d, 'd');
 }
 
-void apply_user_input(fluid_container& container, InputState& input_state, vector<float>& emission_arr)
+void apply_user_input(const sim_config& config, fluid_container& container, InputState& input_state, vector<float>& emission_arr)
 {
     if (isKeyPressed('q') || isKeyPressed('\x03')) 
     {
@@ -62,7 +64,7 @@ void apply_user_input(fluid_container& container, InputState& input_state, vecto
     }
     else
     {
-        add_wind(container, input_state);
-        add_sources(container, input_state, emission_arr);
+        add_wind(config, container, input_state);
+        add_sources(config, container, input_state, emission_arr);
     }
 }
