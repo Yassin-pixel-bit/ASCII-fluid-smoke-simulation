e412310791ac81aa2d23d748bd5365ac6cce22a8
Yassin-pixel-bit <yasyous2021@gmail.com>
2026-03-28 04:06:27 +0200

    refactor: remove dead key-timeout code and poll logic from terminal.cpp

--- src/terminal/terminal.cpp
+++ src/terminal/terminal.cpp
@@ -7,7 +7,6 @@
     #include <sys/ioctl.h>
     #include <unistd.h>
     #include <termios.h>
-    #include <poll.h>
 #endif
 
 int terminal_width = 0;
@@ -16,6 +15,8 @@ int terminal_height = 0;
 // These functions just return copies of the terminal sizes.
 int getTerminalWidth() { return terminal_width; }
 int getTerminalHeight() { return terminal_height; }
+
+static bool key_just_pressed[256] = {false};
 
 void initTerminalSize() {
     #ifdef _WIN32
@@ -49,8 +50,23 @@ void enableANSI() {
 #endif
 }
 
-static int key_timeouts[256] = {0};
-static bool key_just_pressed[256] = {false};
+bool isKeyPressed(char key) {
+#ifdef _WIN32
+    // Windows queries hardware directly
+    int vKey = (key == ' ') ? VK_SPACE : toupper(key);
+    return (GetAsyncKeyState(vKey) & 0x8000) != 0;
+#else
+    return key_just_pressed[(unsigned char)key];
+#endif
+}
+
+bool isKeyJustPressed(char key) {
+#ifdef _WIN32
+    return false; 
+#else
+    return key_just_pressed[(unsigned char)key];
+#endif
+}
 
 #ifndef _WIN32
 static struct termios original_t;
@@ -65,33 +81,27 @@ void updateInput()
         tcgetattr(STDIN_FILENO, &original_t);
         original_saved = true;
         struct termios t = original_t;
+
         t.c_lflag &= ~(ICANON | ECHO);
+
+        t.c_cc[VMIN] = 0;
+        t.c_cc[VTIME] = 0; 
+
         tcsetattr(STDIN_FILENO, TCSANOW, &t);
         init = true;
     }
 
-    for (int i = 0; i < 256; i++) {
-        if (key_timeouts[i] > 0) key_timeouts[i]--;
-    }
-
     std::memset(key_just_pressed, 0, sizeof(key_just_pressed));
 
-    struct pollfd pfd = { STDIN_FILENO, POLLIN, 0 };
-    
-    while (poll(&pfd, 1, 0) > 0) {
-        char buf;
-        if (read(STDIN_FILENO, &buf, 1) > 0) {
-            unsigned char u_buf = (unsigned char)buf;
-            
-            // If the timeout was 0, this is a brand new press
-            if (key_timeouts[u_buf] == 0) {
-                key_just_pressed[u_buf] = true;
-            }
-            
-            // Reset the standard buffer lifespan
-            key_timeouts[u_buf] = 10; 
-        }
+    char buf[32];
+    int bytes_read;
+
+    while ((bytes_read = read(STDIN_FILENO, buf, sizeof(buf))) > 0) 
+    {
+        for (int i = 0; i < bytes_read; i++) 
+            key_just_pressed[(unsigned char)buf[i]] = true;
     }
+
 #endif
 }
 
@@ -102,24 +112,5 @@ void restoreTerminal() {
         // Push the original settings back to the terminal immediately
         tcsetattr(STDIN_FILENO, TCSANOW, &original_t);
     }
-#endif
-}
-
-bool isKeyPressed(char key) {
-#ifdef _WIN32
-    // Windows queries hardware directly
-    int vKey = (key == ' ') ? VK_SPACE : toupper(key);
-    return (GetAsyncKeyState(vKey) & 0x8000) != 0;
-#else
-    // Linux checks if the key still has a positive timeout lifespan
-    return key_timeouts[(unsigned char)key] > 0;
-#endif
-}
-
-bool isKeyJustPressed(char key) {
-#ifdef _WIN32
-    return false; 
-#else
-    return key_just_pressed[(unsigned char)key];
 #endif
 }
